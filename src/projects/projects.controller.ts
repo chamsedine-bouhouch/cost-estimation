523cd3475044255a6b8e5b278165adb5c930d3fb
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-11-10 23:04:55 +0100

    delete project

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Param, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { Project } from './models/project.model';
 import { CreateAnswerDto, CreateProjectDto } from './dtos/create-project.dto';
@@ -7,13 +15,14 @@ import { CreateAnswerDto, CreateProjectDto } from './dtos/create-project.dto';
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
-  @Post()
-  createProject(@Body() createProjectDto: CreateProjectDto): Promise<Project> {
-    return this.projectsService.createProject(createProjectDto);
-  }
   @Get()
   projectList() {
     return this.projectsService.findAll();
+  }
+
+  @Post()
+  createProject(@Body() createProjectDto: CreateProjectDto): Promise<Project> {
+    return this.projectsService.createProject(createProjectDto);
   }
 
   @Get('/:id')
@@ -24,5 +33,10 @@ export class ProjectsController {
   @Patch('/:id')
   update(@Param('id') id: string, @Body() createAnswerDto: CreateAnswerDto) {
     return this.projectsService.addAnswerToProject(id, createAnswerDto);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string) {
+    return this.projectsService.deleteProject(id);
   }
 }
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -9,20 +9,28 @@ export class ProjectsService {
   constructor(
     @InjectModel(Project.name) private readonly projectModel: Model<Project>,
   ) {}
+
+  async findAll(): Promise<Project[]> {
+    return this.projectModel.find().exec();
+  }
+
   async createProject(createProjectDto: CreateProjectDto): Promise<Project> {
     const createdProject = new this.projectModel(createProjectDto);
     return createdProject.save();
   }
-  async findAll(): Promise<Project[]> {
-    return this.projectModel.find().exec();
-  }
+
   async findOne(projectId: string): Promise<Project> {
     return this.projectModel.findById(projectId);
   }
+
   async addAnswerToProject(isProject: string, answer: CreateAnswerDto) {
     await this.projectModel.findByIdAndUpdate(
       { _id: isProject },
       { $push: { answers: answer } },
     );
+  }
+
+  async deleteProject(projectId: string) {
+    return this.projectModel.findByIdAndDelete(projectId);
   }
 }
